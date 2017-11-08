6ca5b492bc131320292289eb05a2d60659368be0
Jared Chapiewsky <chapiewsky@earthlinginteractive.com>
2017-11-07 23:22:46 -0600

    lint server code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,43 +1,46 @@
-import * as express from 'express';
-import * as morgan from 'morgan';
-import * as bodyParser from  'body-parser';
-import * as mustacheExpress from 'mustache-express';
-import api from './routes/api';
-const fs = require('fs');
-const path = require('path');
+import * as express from "express";
+import * as morgan from "morgan";
+import * as bodyParser from "body-parser";
+import * as mustacheExpress from "mustache-express";
+import api from "./routes/api";
 
-const environment = process.env["NODE_ENV"];
+/* tslint:disable */
+const fs = require("fs");
+const path = require("path");
+/* tslint:enable */
+
+const environment = process.env.NODE_ENV;
 
 const app = express();
 
-//include mustache template engine
-app.engine('mst', mustacheExpress());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'mst');
+// include mustache template engine
+app.engine("mst", mustacheExpress());
+app.set("views", __dirname + "/views");
+app.set("view engine", "mst");
 
-//logging
-if(environment === "development") {
-	app.use(morgan('dev'));
+// logging
+if (environment === "development") {
+	app.use(morgan("dev"));
 } else {
-	var logStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), {flags: 'a'});
-	app.use(morgan('combined', {stream: logStream}));
+	const logStream = fs.createWriteStream(path.join(__dirname, "logs", "access.log"), {flags: "a"});
+	app.use(morgan("combined", {stream: logStream}));
 }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // linked client dist directory
-//if (environment === 'production') app.use(express.static(path.join(__dirname, 'public')));
+// if (environment === 'production') app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req: express.Request, res: express.Response) {
-	res.render('index', { title: 'FSSK', message: 'Hello world!' });
+app.get("/", function(req: express.Request, res: express.Response) {
+	res.render("index", { title: "FSSK", message: "Hello world!" });
 });
 
-app.use('/api/', api);
+app.use("/api/", api);
 
 // catch 404 and forward to error handler
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-	let err = new Error('Not Found');
+	const err = new Error("Not Found");
 	next(err);
 });
 
@@ -45,11 +48,11 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render('error');
+	res.render("error");
 });
 
 module.exports = app;
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,8 +1,8 @@
-import * as express from 'express';
+import * as express from "express";
 const router = express.Router();
 
-router.get('/', function (req: express.Request, res: express.Response, next: express.NextFunction) {
-	res.json({ hello: 'world!'});
+router.get("/", function(req: express.Request, res: express.Response, next: express.NextFunction) {
+	res.json({ hello: "world!"});
 });
 
 export default router;
