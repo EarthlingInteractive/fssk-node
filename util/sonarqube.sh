bb0d0dd1f8cd2e3db78b4a7f68e39f33ceb411fb
Jelani Harris <harris@earthlinginteractive.com>
2018-08-29 13:43:38 -0500

    Changed circleci to support a sonarqube post test step, and reintroduced a sonarqube complaint to test pull request functionality.

--- util/sonarqube.sh
+++ util/sonarqube.sh
@@ -1,28 +1,46 @@
 #!/usr/bin/env bash
 
-echo "Looking for LCOV Files ..."
-if [ ! -f client/coverage/lcov.info ]; then
-	echo "Could not find coverage files for client app."
-	exit 1
+# Only run in circleci environments
+if [[ ${CIRCLECI} ]];
+then
+	printf "Circle PR URL: ${CIRCLE_PULL_REQUEST} \n"
+	printf "Circle Branch: $CIRCLE_BRANCH \n"
+	printf "Circle PR Number: ${CIRCLE_PR_NUMBER} \n"
+	printf "Circle Job Name: ${CIRCLE_JOB} \n"
+
+	echo "Looking for LCOV Files ..."
+	if [ ! -f client/coverage/lcov.info ]; then
+		echo "Could not find coverage file for client app."
+		exit 1
+	else
+		echo "Found coverage files for client."
+	fi
+
+	if [ ! -f server/coverage/lcov.info ]; then
+		echo "Could not find coverage file for server app."
+		exit 1
+	else
+		echo "Found coverage files for server."
+	fi
+
+	if [[ ! "${CIRCLE_PULL_REQUEST}" =~ /pull/[0-9]+$ ]]; then
+		echo "Not a pull request."
+	else
+		PR_NUMBER=`echo "${CIRCLE_PULL_REQUEST}" | sed -e 's/.*\///g'`
+
+		echo "Converting LCOV files to have valid relative paths ..."
+		sed -i.bak 's|SF:/opt/src/src|SF:client/src|g' client/coverage/lcov.info
+		sed -i.bak 's|SF:/opt/src/src|SF:server/src|g' server/coverage/lcov.info
+
+		echo "Running SonarQube Scanner ..."
+		docker run -it \
+			-v $(pwd):/root/src \
+			-v $(pwd)/sonar-scanner.properties:/root/sonar-scanner/conf/sonar-scanner.properties \
+			-e SONARQUBE_LOGIN_TOKEN=$SONARQUBE_LOGIN_TOKEN \
+			-e SONARQUBE_GITHUB_OAUTH=$SONARQUBE_GITHUB_OAUTH \
+			fssk-node_sonarqube-scanner sonar-scanner \
+			-X -Dsonar.analysis.mode=preview \
+			-Dsonar.github.pullRequest=${PR_NUMBER}
 else
-	echo "Found coverage files for client."
+  printf "SonarQube analysis step can only be run in Circle CI Environments. \n"
 fi
-
-if [ ! -f server/coverage/lcov.info ]; then
-	echo "Could not find coverage files for server app."
-	exit 1
-else
-	echo "Found coverage files for server."
-fi
-
-echo "Converting LCOV files to have valid paths ..."
-sed -i.bak 's|SF:/opt/src/src|SF:client/src|g' client/coverage/lcov.info
-sed -i.bak 's|SF:/opt/src/src|SF:server/src|g' server/coverage/lcov.info
-
-echo "Running SonarQube Scanner ..."
-docker run -it \
-	-v $(pwd):/root/src \
-	-v $(pwd)/sonar-scanner.properties:/root/sonar-scanner/conf/sonar-scanner.properties \
-	-e SONARQUBE_LOGIN_TOKEN=$SONARQUBE_LOGIN_TOKEN \
-	-e SONARQUBE_GITHUB_OAUTH=$SONARQUBE_GITHUB_OAUTH \
-	fssk-node_sonarqube-scanner sonar-scanner
