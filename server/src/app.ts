57867001d1e01b99e0771bb4e9558c91fddd549b
Steve Brudz <sbrudz@gmail.com>
2019-02-18 14:33:49 -0600

    Log information about server-side errors via winston

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,10 @@ import * as express from "express";
 import * as morgan from "morgan";
 import * as bodyParser from "body-parser";
 import * as mustacheExpress from "mustache-express";
+import * as path from "path";
 import AuthController from "./components/auth";
 import api from "./api";
-import {infoStream, errorStream} from "./util/logger";
+import {infoStream, errorStream, logger} from "./util/logger";
 
 const environment = process.env["NODE_ENV"];
 
@@ -61,14 +62,18 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 
 	// set locals, only providing error in development
 	res.locals.message = err.message || "Unknown error occurred";
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+	res.locals.error = environment === "development" ? err : "";
+	const errStatus = err.status || 500;
 
+	const ipAddr = req.ip || (req.connection && req.connection.remoteAddress) || undefined;
+	const url = req.originalUrl || req.url;
+	logger.error(`${ipAddr} - "${req.method} ${url}" ${res.locals.message} - ${err}`);
 
 	// render the error page
-	res.status(err.status || 500);
+	res.status(errStatus);
 	if (req.get("Content-Type") === "application/json") {
 		const errorJson: any = {
-			code: err.status || 500,
+			code: errStatus,
 			message: res.locals.message,
 			error: res.locals.error,
 		};
