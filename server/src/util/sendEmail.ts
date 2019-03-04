a3273edef3fcb9ac22cf0c2bfbb99e98ad1ba493
Steve Brudz <sbrudz@gmail.com>
2019-03-04 14:31:53 -0600

    Fix issue with EMAIL_SECURE always being set to true
    EMAIL_SECURE needs to be a javascript Boolean but it comes in from the environment variable as a string.  Using the string directly caused javascript to interpret it as a truthy value, even if it was "false". This change fixes that issue.

--- server/src/util/sendEmail.ts
+++ server/src/util/sendEmail.ts
@@ -21,7 +21,7 @@ export default function sendEmail(data: any) {
 	const transporter = nodemailer.createTransport({
 		host: process.env["EMAIL_HOST"],
 		port: process.env["EMAIL_PORT"],
-		secure: process.env["EMAIL_SECURE"],
+		secure: (process.env["EMAIL_SECURE"] === "true"),
 		auth: {
 			user: process.env["EMAIL_USERNAME"],
 			pass: process.env["EMAIL_PASSWORD"],
